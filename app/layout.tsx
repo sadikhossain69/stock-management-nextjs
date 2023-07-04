6b678d74a8b1b38d4a201ed4e74431567c66b9db
sadikhossain69 <sadikhossain433@gmail.com>
2023-07-04 19:40:14 +0600

    dark theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
           {children}
+        </ThemeProvider>
       </body>
     </html>
   )
